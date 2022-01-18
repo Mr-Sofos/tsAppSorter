b3c4da7b38a63cd9134118a61563ac01a41af0c7
Mokhmad <mokhmad.tazbiev@gmail.com>
2022-01-18 04:49:46 +0300

    AddAbstractClass

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-import { Sorter } from './Sorter';
+// import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([12, -1, 3, 5, 15, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Muhammad');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-1);
+linkedList.add(13);
+linkedList.add(14);
+linkedList.sort();
+linkedList.print();
